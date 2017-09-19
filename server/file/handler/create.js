0209da44e91703cc1402cf964859f0a5b99963d0
Judy Tuan <judytuna@gmail.com>
2017-09-19 14:23:07 -0700

    File upload works! If no parentId, use the projectId.

--- server/file/handler/create.js
+++ server/file/handler/create.js
@@ -4,12 +4,6 @@ const createOne = require('../command/create-one')
 const findById = require('../query/find-by-id')
 
 module.exports = (req, res, next) => {
-	console.log("****************")
-	console.log("query")
-	console.log(req.query)
-	console.log("params")
-	console.log(req.params)
-	console.log("WWWWWWWWWWWWWWWW")
 	createOne(req.file, req.query)
 		.then(createdFile => findById(createdFile._id))
 		.then(file => res.json(file))
