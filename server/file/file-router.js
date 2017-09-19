d71a9450d77878db9dc9c49e762ef283622ed235
Judy Tuan <judytuna@gmail.com>
2017-09-19 13:56:01 -0700

    Add create file handler to post.

--- server/file/file-router.js
+++ server/file/file-router.js
@@ -18,6 +18,6 @@ const uploader = multer({
 
 // This should upload the file, however, you'll still need to add another function after this
 // uploader middleware to actually save a record of this file in Mongo
-router.post('/', uploader.single('file'))
+router.post('/', uploader.single('file'), require('./handler/create'))
 
 module.exports = router
